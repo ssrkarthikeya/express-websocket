826b63c5e6991d5719ccbfabcf039a70762a32d1
Sai Sri Rama Kolluri <icegadda@Sais-MacBook-Air.local>
2021-07-29 13:44:48 +0530

    added Self signed SSL Certification

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
-const http = require("http");
+const https = require("https");
 const socketio = require("socket.io");
+const fs = require("fs");
 
 const formatMessage = require("./utils/messages");
 const {
@@ -11,9 +12,17 @@ const {
   userLeave,
 } = require("./utils/users");
 
+const PORT = 3443 || process.env.PORT;
+
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname,'cert/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname,'cert/cert.pem')),
+  },
+  app
+);
+const io = socketio(sslServer);
 
 io.on("connection", (socket) => {
   socket.on("joinRoom", ({ username, room }) => {
@@ -61,10 +70,4 @@ io.on("connection", (socket) => {
 // Set static folder path
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3000 || process.env.PORT;
-
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-
+sslServer.listen(PORT, () => {});
